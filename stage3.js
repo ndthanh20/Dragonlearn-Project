9938dd2c4bdc5253f3e3a1e9185837e16bef0029
Nguyen Thanh <ndthanh20@gmail.com>
2020-04-19 17:16:24 +0700

    Modified code to merge two images into one image

--- stage3.js
+++ stage3.js
@@ -1,5 +1,8 @@
 let currentDroppable = null;
 let bar = holder_1.querySelector('.bar');
+let isPicture = false;
+let endTask = false;
+let count = 1;
 
 bar.onmousedown = function (event) {
 
@@ -14,41 +17,109 @@ bar.onmousedown = function (event) {
     document.addEventListener('mouseup', onMouseUp);
 
     function moveAt(clientY) {
+        if (clientY - shiftY - 246 < 0)
+            if (document.getElementById("movethebarup").hidden == false) {
+                bar.style.top = 170 + 'px';
 
-        
+                onMouseUp();
 
-        if (clientY - shiftY - 246 < 0) {
-            bar.style.top = 170 + 'px';
-            onMouseUp();
+                document.getElementById("holder_2").append(bar);
 
-            document.getElementById("holder_2").append(bar);
+                showPicture(currentDroppable);
 
-            //bar.style.top = 170 + 'px';
-            showPicture(currentDroppable);
-            
-            return;
-        }
-        if (clientY - shiftY - 248 > 340) {
-            bar.style.top = 340 + 'px';
-            return
-        }
+                isPicture = true;
+
+                if (!document.getElementById("movethebarup").hidden) {
+                    document.getElementById("movethebarup").hidden = true;
+                    document.getElementById("movethebardown").hidden = false;
+                }
+                else {
+                    document.getElementById("movethebardown").hidden = true;
+                    document.getElementById("movethebarup").hidden = false;
+                }
+
+                return;
+            }
+            else {
+                bar.style.top = 0 + 'px';
+                return;
+            }
+        if (clientY - shiftY - 248 > 340)
+            if (document.getElementById("movethebardown").hidden == false) {
+                bar.style.top = 340 + 'px';
+                onMouseUp();
+
+                showPicture(currentDroppable);
+
+                isPicture = true;
+
+                if (!document.getElementById("movethebarup").hidden) {
+                    document.getElementById("movethebarup").hidden = true;
+                    document.getElementById("movethebardown").hidden = false;
+                }
+                else {
+                    document.getElementById("movethebardown").hidden = true;
+                    document.getElementById("movethebarup").hidden = false;
+                }
+
+                bar.style.visibility = "hidden";
+
+                endTask = true;
+
+                return
+            }
         bar.style.top = clientY - shiftY - 248 + 'px';
 
-        console.log(bar.style.top);
+    }
+    function showPicture(elem) {
 
+        var image_1 = document.createElement("div");
 
-        if (bar.style.top == '0px') {
-     
-        }
+        image_1.className = 'image_1';
+
+        var urlI = 'url("/Images/51_' + count + '.png")';
+
+        image_1.style.backgroundImage = urlI;
+
+        image_1.id = 'sprite_' + count;
+
+        count += 1;
+
+        image_1.style.width = 180 + 'px';
+
+        image_1.style.height = 180 + 'px';
+
+        elem.append(image_1);
+
 
     }
 
     function onMouseMove(event) {
 
 
-
         moveAt(event.clientY);
 
+
+        if (endTask) {
+
+
+            document.getElementById("sprite_1").style.animationName = "sprite_1";
+
+            document.getElementById("sprite_1").style.animationDuration = "2s";
+
+            document.getElementById("sprite_1").style.animationFillMode = "both";
+
+            document.getElementById("sprite_2").style.animationName = "sprite_2";
+
+            document.getElementById("sprite_2").style.animationDuration = "2.5s";
+
+            document.getElementById("sprite_2").style.animationFillMode = "both";
+
+            document.getElementById("sprite_2").style.animationTimingFunction = "ease";
+            onMouseUp();
+
+            return;
+        }
 
 
         bar.style.visibility = "hidden";
@@ -61,7 +132,6 @@ bar.onmousedown = function (event) {
         if (!elemBelow) return;
 
         let droppableBelow = elemBelow.closest('.droppable');
-
 
 
         if (currentDroppable != droppableBelow) {
@@ -79,7 +149,7 @@ bar.onmousedown = function (event) {
                 enterDroppable(currentDroppable);
             }
             //console.log(currentDroppable);
-            
+
             /*if(bar.style.top == '340px'){
 
             }*/
@@ -88,32 +158,14 @@ bar.onmousedown = function (event) {
     function enterDroppable(elem) {
 
         //elem.style.backgroundImage = "url('./Images/44.png')";
-        elem.style.background = 'pink';
+        elem.style.background = 'yellow';
     }
 
     function leaveDroppable(elem) {
-        elem.style.backgroundImage = '';
-    }
-
-    function showPicture(elem) {
-
-       /* var image_1 = document.createElement("div");
-
-        image_1.className = 'image_1';
-
-        image_1.style.backgroundImage = "url('./Images/51_2.png')";
-
-        image_1.style.width=180 +'px';
-
-        image_1.style.height= 180+'px';
-
-        elem.append(image_1);*/
-
-        elem.style.backgroundImage= "url('./Images/51_2.png')";
-
-        /*$(document).ready(function(){
-            $("#first").append("<img style='height:100px ; width:100px' src='./Images/51_2.png'></img>");
-        });*/
+        if (!isPicture)
+            elem.style.backgroundImage = '';
+        else
+            isPicture = false;
     }
 
 
