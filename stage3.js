ebb6390654d9a265217f6e484057e1da6c6a4a42
Nguyen Thanh <ndthanh20@gmail.com>
2020-05-18 00:22:41 +0700

    Stage3: Fix move bar after change task

--- stage3.js
+++ stage3.js
@@ -1,179 +1,202 @@
 let currentDroppable = null;
-let bar = holder_1.querySelector('.bar1');
 let isPicture = false;
 let endTask = false;
 let count = 1;
 var sceneNumber = 0;
+var task = 0, bar;
 
-bar.onmousedown = function (event) {
+var control = (function () {
+    function moveBar() {
+        let shiftY = event.clientY - bar.getBoundingClientRect().top;
+        switch (task){
+            case 1:
+                
+        }
+        //bar.style.position = 'absolute';
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
 
-    console.log(bar);
+        function moveAt(clientY) {
+            if (clientY - shiftY - 246 < 0)
+                if (document.getElementById("movethebarup").hidden == false) {
+                    bar.style.top = 170 + 'px';
+                    onMouseUp();
 
-    let shiftY = event.clientY - bar.getBoundingClientRect().top;
+                    document.getElementById("holder_2").append(bar);
 
-    bar.style.position = 'absolute';
+                    showPicture(currentDroppable);
 
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
+                    isPicture = true;
 
-    function moveAt(clientY) {
-        if (clientY - shiftY - 246 < 0)
-            if (document.getElementById("movethebarup").hidden == false) {
-                bar.style.top = 170 + 'px';
-
-                onMouseUp();
-
-                document.getElementById("holder_2").append(bar);
-
-                showPicture(currentDroppable);
-
-                isPicture = true;
-
-                if (!document.getElementById("movethebarup").hidden) {
-                    document.getElementById("movethebarup").hidden = true;
-                    document.getElementById("movethebardown").hidden = false;
+                    if (!document.getElementById("movethebarup").hidden) {
+                        document.getElementById("movethebarup").hidden = true;
+                        document.getElementById("movethebardown").hidden = false;
+                    }
+                    else {
+                        document.getElementById("movethebardown").hidden = true;
+                        document.getElementById("movethebarup").hidden = false;
+                    }
+                    return;
                 }
                 else {
-                    document.getElementById("movethebardown").hidden = true;
-                    document.getElementById("movethebarup").hidden = false;
+                    bar.style.top = 0 + 'px';
+                    return;
                 }
+            if (clientY - shiftY - 248 > 340)
+                if (document.getElementById("movethebardown").hidden == false) {
+                    bar.style.top = 340 + 'px';
+                    onMouseUp();
+
+                    showPicture(currentDroppable);
+
+                    isPicture = true;
+
+                    if (!document.getElementById("movethebarup").hidden) {
+                        document.getElementById("movethebarup").hidden = true;
+                        document.getElementById("movethebardown").hidden = false;
+                    }
+                    else {
+                        document.getElementById("movethebardown").hidden = true;
+                        document.getElementById("movethebarup").hidden = false;
+                    }
+                    bar.style.visibility = "hidden";
+                    endTask = true;
+                    return
+                }
+                else {
+                    bar.style.top = 340 + 'px';
+                    return;
+                }
+            bar.style.top = clientY - shiftY - 248 + 'px';
+        }
+        function showPicture(elem) {
+            var image_1 = document.createElement("div");
+            image_1.className = 'image_1';
+
+            var urlI = 'url("./Images/51_' + count + '.png")';
+
+            image_1.style.backgroundImage = urlI;
+
+            image_1.id = 'sprite_' + count;
+
+            count += 1;
+
+            image_1.style.width = 180 + 'px';
+
+            image_1.style.height = 180 + 'px';
+
+            elem.append(image_1);
+
+            elem.style.visibility = "hidden";
+
+            image_1.style.visibility = "visible";
+        }
+        function onMouseMove(event) {
+            moveAt(event.clientY);
+            if (endTask) {
+                var x = document.getElementById("task_1").querySelectorAll(".square");
+
+                for (var i = 0; i < x.length; i++) {
+                    x[i].style.visibility = "hidden";
+                }
+                document.getElementById("sprite_1").style.animationName = "sprite_1";
+                document.getElementById("sprite_1").style.animationDuration = "2s";
+                document.getElementById("sprite_1").style.animationFillMode = "both";
+                document.getElementById("sprite_1").style.animationTimingFunction = "ease";
+
+                document.getElementById("sprite_2").style.animationName = "sprite_2";
+                document.getElementById("sprite_2").style.animationDuration = "2s";
+                document.getElementById("sprite_2").style.animationFillMode = "both";
+                document.getElementById("sprite_2").style.animationTimingFunction = "ease";
+
+                document.getElementById("bead2").style.animationName = "ball1";
+                document.getElementById("bead2").style.animationDelay = "2s";
+                document.getElementById("bead2").style.webkitAnimationDuration = "2s";
+                document.getElementById("bead2").style.animationTimingFunction = "ease";
+                document.getElementById("bead2").style.animationFillMode = "both";
+
+                document.getElementById("holder_1").querySelector("#first").style.visibility = "hiiden";
+
+                setTimeout(function () {
+                    document.getElementById("task_1").style.display = "none";
+
+                    document.getElementById("task_2").style.display = "block";
+
+                    document.querySelector(".btn_play").style.display = "block";
+
+                    document.getElementById("main").style.filter = "blur(2px)";
+                    setUpEventListener();
+                }, 3000);
 
                 return;
             }
-            else {
-                bar.style.top = 0 + 'px';
-                return;
-            }
-        if (clientY - shiftY - 248 > 340)
-            if (document.getElementById("movethebardown").hidden == false) {
-                bar.style.top = 340 + 'px';
-                onMouseUp();
+            bar.style.visibility = "hidden";
 
-                showPicture(currentDroppable);
+            let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
 
-                isPicture = true;
+            bar.style.visibility = "visible";
 
-                if (!document.getElementById("movethebarup").hidden) {
-                    document.getElementById("movethebarup").hidden = true;
-                    document.getElementById("movethebardown").hidden = false;
+            if (!elemBelow) return;
+
+            let droppableBelow = elemBelow.closest('.square');
+
+            if (currentDroppable != droppableBelow) {
+                if (currentDroppable) {
+                    leaveDroppable(currentDroppable);
                 }
-                else {
-                    document.getElementById("movethebardown").hidden = true;
-                    document.getElementById("movethebarup").hidden = false;
+                currentDroppable = droppableBelow;
+
+                if (currentDroppable) {
+                    enterDroppable(currentDroppable);
                 }
-                bar.style.visibility = "hidden";
-                endTask = true;
-                return
             }
-        bar.style.top = clientY - shiftY - 248 + 'px';
+        }
+        function enterDroppable(elem) {
+            elem.style.background = '#ffffcc';
+        }
+        function leaveDroppable(elem) {
+            if (!isPicture)
+                elem.style.backgroundImage = '';
+            else
+                isPicture = false;
+        }
+        function onMouseUp() {
+            document.removeEventListener('mouseup', onMouseUp);
+            document.removeEventListener('mousemove', onMouseMove);
+        }
+    };
+    //newnewnew
+    var handleStart = function () {
+        var btn_play = document.querySelector(".btn_play");
+        if (btn_play)
+            btn_play.style.display = "none";
+        document.getElementById("main").style.filter = "none";
     }
-    function showPicture(elem) {
-        var image_1 = document.createElement("div");
-
-        image_1.className = 'image_1';
-
-        var urlI = 'url("./Images/51_' + count + '.png")';
-
-        image_1.style.backgroundImage = urlI;
-
-        image_1.id = 'sprite_' + count;
-
-        count += 1;
-
-        image_1.style.width = 180 + 'px';
-
-        image_1.style.height = 180 + 'px';
-
-        elem.append(image_1);
-
-        elem.style.visibility = "hidden";
-
-        image_1.style.visibility = "visible";
-    }
-
-    function onMouseMove(event) {
-
-        moveAt(event.clientY);
-
-        if (endTask) {
-
-            onMouseUp();
-
-            var x = document.getElementById("task_1").querySelectorAll(".droppable1");
-
-            for (var i = 0; i < x.length; i++) {
-                x[i].style.visibility = "hidden";
-            }
-            document.getElementById("sprite_1").style.animationName = "sprite_1";
-            document.getElementById("sprite_1").style.animationDuration = "2s";
-            document.getElementById("sprite_1").style.animationFillMode = "both";
-            document.getElementById("sprite_1").style.animationTimingFunction = "ease";
-
-            document.getElementById("sprite_2").style.animationName = "sprite_2";
-            document.getElementById("sprite_2").style.animationDuration = "2s";
-            document.getElementById("sprite_2").style.animationFillMode = "both";
-            document.getElementById("sprite_2").style.animationTimingFunction = "ease";
-
-            document.getElementById("bead2").style.animationName = "ball1";
-            document.getElementById("bead2").style.animationDelay = "2s";
-            document.getElementById("bead2").style.webkitAnimationDuration = "2s";
-            document.getElementById("bead2").style.animationTimingFunction = "ease";
-            document.getElementById("bead2").style.animationFillMode = "both";
-
-            document.getElementById("holder_1").querySelector("#first").style.visibility = "hiiden";
-
-            setTimeout(function () {
-                document.getElementById("task_1").style.display = "none";
-
-                document.getElementById("task_2").style.display = "block";
-
+    var setUpEventListener = function () {
+        task++;
+        document.querySelector(".btn_play").addEventListener("click", handleStart);
+        switch (task) {
+            case 1:
+                bar = holder_1.querySelector('.bar1');
+                break;
+            case 2:
                 bar = holder_1_2.querySelector('.bar2');
-            }, 3000);
-            
-            return;
+                break;
+            default:
+                break;
         }
-
-        bar.style.visibility = "hidden";
-
-        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-
-        bar.style.visibility = "visible";
-
-        if (!elemBelow) return;
-
-        let droppableBelow = elemBelow.closest('.droppable1');
-
-        if (currentDroppable != droppableBelow) {
-            if (currentDroppable) {
-                leaveDroppable(currentDroppable);
-            }
-            currentDroppable = droppableBelow;
-
-            if (currentDroppable) {
-                enterDroppable(currentDroppable);
-            }
+        bar.ondragstart = function () {
+            return false;
+        };
+        bar.addEventListener("mousedown", moveBar);
+    }
+    return {
+        init: function () {
+            setUpEventListener();
         }
     }
-    function enterDroppable(elem) {
-
-        //elem.style.backgroundImage = "url('./Images/44.png')";
-        elem.style.background = '#ffffcc';
-    }
-    function leaveDroppable(elem) {
-        if (!isPicture)
-            elem.style.backgroundImage = '';
-        else
-            isPicture = false;
-    }
-    function onMouseUp() {
-        document.removeEventListener('mouseup', onMouseUp);
-        document.removeEventListener('mousemove', onMouseMove);
-    }
-};
-
-bar.ondragstart = function () {
-    return false;
-};
+})()
+control.init();
 function playSound() {
     if (!document.getElementById("movethebarup").hidden) {
         var x = document.getElementById("up");
